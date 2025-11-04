3416e9f0eb2c3e4a001ff366aeba35e4204dbbe9
Branson Clendenen <bclendenen@csub.edu>
2025-11-04 09:49:18 -0800

    Added Upgrades View

--- 3390project3/Scripts/main_menu.gd
+++ 3390project3/Scripts/main_menu.gd
@@ -12,3 +12,6 @@ func _on_start_game_pressed() -> void:
 
 func _on_sign_out_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Login.tscn")
+
+func _on_upgrades_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Upgrades.tscn")
