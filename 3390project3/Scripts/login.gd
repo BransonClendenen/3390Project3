4191bc694436065bde4ca938e50c0380593fc67a
paolajoj <porozco5@csub.edu>
2025-11-05 17:47:44 -0800

    login.gd initial test
    xDDDDDD

--- 3390project3/Scripts/login.gd
+++ 3390project3/Scripts/login.gd
@@ -1,13 +1,18 @@
 extends Node2D
 
+@onready var status_label = $StatusLabel
+
+@onready var password_field = $passwordField
+@onready var username_field = $usernameField
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
 func _on_create_account_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
 
