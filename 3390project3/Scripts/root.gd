545d965d4c1ff4008d91b3d409c83b3bc33a393c
Branson Clendenen <bclendenen@csub.edu>
2025-11-10 18:06:58 -0800

    IT WORKS YEEES

--- 3390project3/Scripts/root.gd
+++ 3390project3/Scripts/root.gd
@@ -3,8 +3,6 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	await get_tree().process_frame
-	SceneManager.load_ui("res://Scenes/UI/Login.tscn")
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- 3390project3/Scripts/scene_manager.gd
+++ 3390project3/Scripts/scene_manager.gd
@@ -7,25 +7,18 @@ extends Node
 @onready var overlay_stack: Array = []
 
 @onready var game_layer: Node2D = null
-@onready var ui_layer: CanvasLayer = null
-@onready var overlay_layer: CanvasLayer = null
+@onready var ui_layer: Control = null
+@onready var overlay_layer: Control = null
 
 func _ready():
-	load_main_scene()
-
-func load_main_scene():
-	var packed_root = load("res://Scenes/Game/Root.tscn")
-	root_node = packed_root.instantiate()
-	get_tree().root.add_child(root_node)
-	print("packed scene_root")
-	
-	await get_tree().process_frame
 	setup_layers()
+	load_ui("res://Scenes/UI/Login.tscn")
 
 func setup_layers():
+	root_node = get_tree().current_scene
 	game_layer = root_node.get_node("GameLayer")
-	ui_layer = root_node.get_node("UILayer")
-	overlay_layer = root_node.get_node("OverlayLayer")
+	ui_layer = root_node.get_node("UILayer/UIContainer")
+	overlay_layer = root_node.get_node("OverlayLayer/OverlayContainer")
 	print(game_layer)
 	print(ui_layer)
 	print(overlay_layer)
@@ -44,32 +37,13 @@ func load_scene(scene_path: String):
 	game_scene = new_game_layer
 	print("Loaded main scene:", scene_path)
 
-
 func load_ui(scene_path: String):
-	print("Main children:", root_node.get_children())
-	print("laod ui entered:")
-	print(scene_path)
-	print(root_node) 
-	print(ui_layer)
 	#ui_layer.get_children().map(func(child): child.queue_free())
 	
 	var new_ui_layer = load(scene_path).instantiate()
 	new_ui_layer.size = get_viewport().size
 	ui_layer.add_child(new_ui_layer)
 	
-	print("Loaded new ui:", scene_path)
-	print("UI children:", ui_layer.get_children())
-	print("UILayer visible:", ui_layer.visible, "layer:", ui_layer.layer)
-	
-	#print("UI root:", ui_layer, "position:", ui_layer.position, "size:", ui_layer.size)
-	for node in ui_layer.get_children():
-		print(" -", node.name, "pos:", node.position, "size:", node.size)
-	
-	print("UILayer visible:", ui_layer.visible)
-	print("UI child visible:", new_ui_layer.visible)
-	print("UILayer layer:", ui_layer.layer)
-
-
 
 func load_overlay(scene_path: String):
 	#var overlay_layer = root_node.get_node("OverlayLayer")
