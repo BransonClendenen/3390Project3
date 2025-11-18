4037d4e2356f859b13c5638ca84aa87f737bf61e
Branson Clendenen <bclendenen@csub.edu>
2025-11-17 16:51:31 -0800

    Added Enemy Death on Collison

--- 3390project3/Scripts/enemy_1.gd
+++ 3390project3/Scripts/enemy_1.gd
@@ -10,4 +10,7 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	velocity = (player.global_position - global_position).normalized() * SPEED
 	
+	look_at(player.global_position)
+	
 	move_and_slide()
+	
