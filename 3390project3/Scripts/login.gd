493f9e77845fd474296e72bd15133a856b8227a8
Branson Clendenen <bclendenen@csub.edu>
2025-11-24 11:02:19 -0800

    Disable login api stuff

--- 3390project3/Scripts/login.gd
+++ 3390project3/Scripts/login.gd
@@ -1,15 +1,15 @@
 extends Control
 
-@onready var status_label = $StatusLabel
-@onready var password_field = $passwordField
-@onready var username_field = $usernameField
-@onready var http_request = $HTTPRequest
+#@onready var status_label = $StatusLabel
+#@onready var password_field = $passwordField
+#@onready var username_field = $usernameField
+#@onready var http_request = $HTTPRequest
 
-var API_BASE:="http://localhost:3000/"
-var auth_token:=""
+#var API_BASE:="http://localhost:3000/"
+#var auth_token:=""
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	http_request.request_completed(_on_request_completed)
+#func _ready() -> void:
+	#http_request.request_completed(_on_request_completed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -22,33 +22,33 @@ func _on_sign_in_pressed() -> void:
 	SceneManager.load_ui("res://Scenes/UI/MainMenu.tscn")
 
 # func login PAOLAPAOLAPAOLA
-func _on_LoginButton_pressed():
-	# collect username and password from fields
-	var username = username_field.text
-	var password = password_field.text
-
-	var payload = {
-		"username": username,
-		"password": password
-	}
-
-	var json = JSON.stringify(payload)
-
-	var url = "http://localhost:3000/login" # pls remember to change to api endpoint later
-	var headers = ["Content-Type: application/json"]
-
-	var http_request = HTTPRequest.new()
-	add_child(http_request)
-	http_request.request_completed.connect(_on_request_completed)
-	http_request.request(url, headers, HTTPClient.METHOD_POST, json)
-
-# clear field when entering anything atp lolol
-# handling api response below; this is supposed to send JSON payload to da API and shows results inside the label
-func _on_request_completed(result, response_code, headers, body):
-	var data = JSON.parse_string(body.get_string_from_utf8())
-
-	if response_code == 200:
-		status_label.text = "successful login, %s" % data["username"]
-		# TODO: store token or switch scene
-	else:
-		status_label.text = "Error %d: %s" % [response_code, str(data)]
+#func _on_LoginButton_pressed():
+	## collect username and password from fields
+	#var username = username_field.text
+	#var password = password_field.text
+#
+	#var payload = {
+		#"username": username,
+		#"password": password
+	#}
+#
+	#var json = JSON.stringify(payload)
+#
+	#var url = "http://localhost:3000/login" # pls remember to change to api endpoint later
+	#var headers = ["Content-Type: application/json"]
+#
+	#var http_request = HTTPRequest.new()
+	#add_child(http_request)
+	#http_request.request_completed.connect(_on_request_completed)
+	#http_request.request(url, headers, HTTPClient.METHOD_POST, json)
+#
+## clear field when entering anything atp lolol
+## handling api response below; this is supposed to send JSON payload to da API and shows results inside the label
+#func _on_request_completed(result, response_code, headers, body):
+	#var data = JSON.parse_string(body.get_string_from_utf8())
+#
+	#if response_code == 200:
+		#status_label.text = "successful login, %s" % data["username"]
+		## TODO: store token or switch scene
+	#else:
+		#status_label.text = "Error %d: %s" % [response_code, str(data)]
