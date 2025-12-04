618f6e1e86d2d6610cd4bda967b140aecc1a7f06
paolajoj <porozco5@csub.edu>
2025-12-03 17:32:16 -0800

    go away ho

--- 3390project3/Scripts/login.gd
+++ 3390project3/Scripts/login.gd
@@ -34,7 +34,7 @@ func _on_LoginButton_pressed() -> void:
 		"password": password_field.text
 	}
 	
-	status.text = "you shouldnt be logging in with a loser like me, or should you"
+	status.text = "logging in"
 	var headers = ["Content-Type: application/json"]
 	var url = API_BASE + "/login"
 	
@@ -113,34 +113,3 @@ func _on_sign_in_pressed() -> void:
 	_on_LoginButton_pressed()
 	emit_signal("set_username",username_field.text)
 	SceneManager.load_ui("res://Scenes/UI/MainMenu.tscn")
-
-#func _on_LoginButton_pressed():
-	# collect username and password from fields
-#	var username = username_field.text
-#	var password = password_field.text
-
-#	var payload = {
-#		"username": username,
-#		"password": password
-#	}
-
-#	var json = JSON.stringify(payload)
-
-#	var url = "http://localhost:3000/login" # pls remember to change to api endpoint later
-#	var headers = ["Content-Type: application/json"]
-
-#	var http_request = HTTPRequest.new()
-#	add_child(http_request)
-#	http_request.request_completed.connect(_on_request_completed)
-#	http_request.request(url, headers, HTTPClient.METHOD_POST, json)
-
-# clear field when entering anything atp lolol
-# handling api response below; this is supposed to send JSON payload to da API and shows results inside the label
-#func _on_request_completed(result, response_code, headers, body):
-	#var data = JSON.parse_string(body.get_string_from_utf8())
-
-	#if response_code == 200:
-		#status_label.text = "successful login, %s" % data["username"]
-		## TODO: store token or switch scene
-	#else:
-		#status_label.text = "Error %d: %s" % [response_code, str(data)]
